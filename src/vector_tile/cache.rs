a0277d648efd7404a45740d242baae630a1c53c1
Noah Hüsser <yatekii@yatekii.ch>
2019-06-26 01:29:28 +0200

    Tiles are now loaded in parallel in the background.

--- src/vector_tile/cache.rs
+++ src/vector_tile/cache.rs
@@ -1,23 +1,74 @@
+use crate::vector_tile::math::TileId;
 use std::collections::HashMap;
 use crate::vector_tile::math;
 use rayon::prelude::*;
+use std::thread::{
+    JoinHandle,
+    spawn,
+};
+use std::sync::mpsc::{
+    channel,
+    Sender,
+    Receiver,
+};
 
 pub struct TileCache {
     cache: HashMap<math::TileId, Tile>,
+    loaders: Vec<(u64, JoinHandle<std::vec::Vec<crate::vector_tile::transform::Layer>>, TileId)>,
+    channel: (Sender<u64>, Receiver<u64>),
+    id: u64,
 }
 
 impl TileCache {
     pub fn new() -> Self {
         Self {
             cache: HashMap::new(),
+            loaders: vec![],
+            channel: channel(),
+            id: 0,
         }
     }
 
     pub fn fetch_tile(&mut self, tile_id: &math::TileId) {
+        match self.channel.1.try_recv() {
+            Ok(id) => {
+                let mut found = false;
+                let mut i = 0;
+                for loader in &self.loaders {
+                    if loader.0 == id {
+                        i += 1;
+                        found = true;
+                        break;
+                    }
+                }
+                if found {
+                    let loader = self.loaders.remove(i);
+                    if let Ok(layers) = loader.1.join() {
+                        self.cache.insert(loader.2, Tile { layers: layers });
+                    } else {
+                        log::error!("Failed to join tile loader thread.");
+                    }
+                }
+            },
+            Err(_) => ()
+        }
+        
+        let id = self.id;
+        self.id += 1;
+        
         if !self.cache.contains_key(&tile_id) {
-            let data = crate::vector_tile::fetch_tile_data(&tile_id);
-            let layers = crate::vector_tile::vector_tile_to_mesh(&tile_id, &data);
-            self.cache.insert(tile_id.clone(), Tile { layers: layers });
+            let tile_id_clone = tile_id.clone();
+            let tx = self.channel.0.clone();
+            self.loaders.push((
+                id,
+                spawn(move|| {
+                    let data = crate::vector_tile::fetch_tile_data(&tile_id_clone);
+                    let layers = crate::vector_tile::vector_tile_to_mesh(&tile_id_clone, &data);
+                    tx.send(id).unwrap();
+                    layers
+                }),
+                tile_id.clone(),
+            ));
         }
     }
 
@@ -27,7 +78,7 @@ impl TileCache {
 
     pub fn fetch_tiles(&mut self, screen: &math::Screen) {
         let tile_field = screen.get_tile_boundaries_for_zoom_level(8).iter().collect::<Vec<_>>();
-        let data = tile_field.par_iter().filter_map(|tile_id| {
+        let data = tile_field.iter().filter_map(|tile_id| {
             if !self.cache.contains_key(&tile_id) {
                 let data = crate::vector_tile::fetch_tile_data(&tile_id);
                 let layers = crate::vector_tile::vector_tile_to_mesh(&tile_id, &data);
